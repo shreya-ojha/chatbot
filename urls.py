1c86253764c8ed7aa5a76b67d514c95426475125
Shreya Ojha <125897857+shreya-ojha@users.noreply.github.com>
2025-12-02 19:18:11 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from chatbot import views
@@ -16,3 +15,4 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
